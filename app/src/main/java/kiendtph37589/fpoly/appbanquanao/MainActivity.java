d44626201a82f9395a3e22a7bc6b0d6c7798337a
ThanhCong1008 <lecong10082004@gmail.com>
2023-11-17 15:54:59 +0700

    kien pull

--- app/src/main/java/kiendtph37589/fpoly/appbanquanao/MainActivity.java
+++ app/src/main/java/kiendtph37589/fpoly/appbanquanao/MainActivity.java
@@ -2,13 +2,49 @@ package kiendtph37589.fpoly.appbanquanao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView img_Logo;
+    private static final int FADE_DURATION = 2000; // Thời gian hiệu ứng hiển thị (milliseconds)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            img_Logo = findViewById(R.id.img_logo);
+// Tạo hiệu ứng hiển thị dần dần
+            Animation fadeInAnimation = new AlphaAnimation(0, 1);
+            fadeInAnimation.setDuration(FADE_DURATION);
+            fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    img_Logo.setVisibility(View.VISIBLE);
+                }
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    // Sau khi hiệu ứng kết thúc, chuyển đến màn hình chính
+                    // Ví dụ: startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                    finish();
+                }
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                    // Không cần xử lý
+                }
+            });
+            img_Logo.startAnimation(fadeInAnimation);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(MainActivity.this, DangNhap.class));
+                    finish();
+                }
+            },FADE_DURATION);
+        }
     }
-}
--- app/src/main/java/kiendtph37589/fpoly/appbanquanao/add.java
+++ app/src/main/java/kiendtph37589/fpoly/appbanquanao/add.java
@@ -4,4 +4,5 @@ public class add {
 
     //kjhkjhj
     //fghjg
+    //kitrghghcgsd
 }
