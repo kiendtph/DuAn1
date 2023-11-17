f00e0e439d41245287e745bf7e272b22688afb37
kiendtph <dinhtrongkien2004@gmail.com>
2023-11-17 15:13:41 +0700

    cong cmt

--- app/src/main/java/kiendtph37589/fpoly/appbanquanao/DangKy.java
+++ app/src/main/java/kiendtph37589/fpoly/appbanquanao/DangKy.java
@@ -10,5 +10,6 @@ public class DangKy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
+        //fghjklkjhfgdgfhj
     }
 }
